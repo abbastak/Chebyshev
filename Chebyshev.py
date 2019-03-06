593fadfc762711f62668c180536d1bed60a676ac
Abbas TAK <19572200+abbastak@users.noreply.github.com>
2019-03-06 22:02:03 +0330

    v.01
    !

---
+++ Chebyshev.py
@@ -0,0 +1,37 @@
+try:
+    x = float(input("factorial ra vared kon: "))
+    y = float(input("amel aval ra vared kon: "))
+    if y < 0:
+        print("faghat a\'dad mosbat bzn baraye amel aval")
+except:
+    print("mosbat bzn")
+
+if y == 0:
+    y = 5
+else:
+    pass
+
+try:
+    tavan = 1
+    l = []
+    z = y
+    while True:
+        t = x / z
+        #print(t)
+        tavan += 1
+        #print(tavan)
+        z = y**tavan
+        t = int(t)
+        l.append(t)
+        if t == 0:
+            print(l)
+            break
+
+except Exception as err:
+    print("error", err)
+
+j = 0
+for i in l:
+    j += i
+
+print(j)
