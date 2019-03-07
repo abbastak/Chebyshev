498126caaf9ae0cc170b255c79bdde5c11cd7bb5
Abbas TAK <19572200+abbastak@users.noreply.github.com>
2019-03-07 23:21:14 +0330

    v 0.2.0

--- Chebyshev.py
+++ Chebyshev.py
@@ -1,35 +1,33 @@
 try:
-    x = float(input("factorial ra vared kon: "))
-    y = float(input("amel aval ra vared kon: "))
-    if y < 0:
-        print("faghat a\'dad mosbat bzn baraye amel aval")
-except:
-    print("mosbat bzn")
+    x = int(input("factorial ra vared kon: "))
+    y = int(input("amel aval ra vared kon: "))
+    if y > 0 or y == 0 and x > 0 or x == 0:
+        print("wait a sec")
+    else:
+        print("Adad mosbat bzn")
+        exit()
+except ValueError:
+    print("Adad bedon a\'shar VARED kn")
+    exit()
 
 if y == 0:
     y = 5
-else:
-    pass
 
-try:
-    tavan = 1
-    l = []
-    z = y
-    while True:
-        t = x / z
-        tavan += 1
-        z = y**tavan
-        t = int(t)
-        l.append(t)
-        if t == 0:
-            print(l)
-            break
-
-except Exception as err:
-    print("error", err)
+tavan = 1
+li = []
+z = y
+while True:
+    t = x / z
+    tavan += 1
+    z = y**tavan
+    t = int(t)
+    li.append(t)
+    if t == 0:
+        print(li)
+        break
 
 j = 0
-for i in l:
+for i in li:
     j += i
 
 print("answer: ", j)
